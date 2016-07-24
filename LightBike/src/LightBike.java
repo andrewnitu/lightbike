cacc3de35729bbb86f1f3513a76b7042e54bcf73
Andrew Nitu <andrewnitu@gmail.com>
2016-07-24 19:13:57 -0400

    Add game start class & graphics implementation code

---
+++ LightBike/src/LightBike.java
@@ -0,0 +1,29 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+
+public class LightBike extends JFrame {
+	public static final String WINDOW_TITLE = "Java LightBike";
+
+	public LightBike() {
+		add(new MainMenu());
+
+		setResizable(false);
+		pack();
+
+		setTitle(WINDOW_TITLE);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable(){
+			@Override
+			public void run() {
+				JFrame ex = new LightBike();
+				ex.setVisible(true);
+			}
+		});
+	}
+}
--- LightBike/src/MainMenu.java
+++ LightBike/src/MainMenu.java
@@ -1,11 +1,45 @@
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
 
-public class MainMenu {
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-	public static void main(String[] args) {
+@SuppressWarnings({"unused","serial"})
 
+public class MainMenu extends JPanel implements ActionListener {
+
+	//Game window will be 720p
+	public static final int WINDOW_WIDTH = 1280;
+	public static final int WINDOW_HEIGHT = 720;
+
+	public MainMenu() {
+		addKeyListener(new TAdapter());
+		setBackground(Color.black);
+		setFocusable(true);
+		
+		setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+		
+		
+	}
+	
+	@Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+    }
+	
+	private void startGame() {
+		
 	}
 
 }
