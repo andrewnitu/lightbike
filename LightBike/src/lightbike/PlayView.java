a345ac55334a3e0484096aa0f4b28ef874624c22
Andrew Nitu <andrewnitu@gmail.com>
2017-02-03 01:50:50 -0500

    Add PlayView.java
    Driving class for the game

---
+++ LightBike/src/lightbike/PlayView.java
@@ -0,0 +1,18 @@
+package lightbike;
+
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PlayView extends JPanel {
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		//updateGame();
+		
+		Toolkit.getDefaultToolkit().sync();
+	}
+}
