1d57a10c3479647cf423cc08b69c9c3dcb6fb17c
Andrew Nitu <andrewnitu@gmail.com>
2017-02-11 02:59:56 -0500

    Add PlayView as a possible view in Application.java

--- src/main/Application.java
+++ src/main/Application.java
@@ -8,49 +8,51 @@ import java.awt.EventQueue;
 @SuppressWarnings("serial")
 public class Application extends JFrame {
 	// Reference http://stackoverflow.com/questions/6175899/how-to-change-card-layout-panels-from-another-panel
-	
+
 	private CardLayout cardLayout = new CardLayout();
 	private JPanel cards = new JPanel(cardLayout);
 	public static final int WINDOW_WIDTH = 1280;
 	public static final int WINDOW_HEIGHT = 720;
-	
+
 	private static final String WINDOW_TITLE = "LightBike";
-	
+
 	public Application() {
 		initialize();
 	}
-	
+
 	private void initialize() {
 		MainMenuView mainMenuView = new MainMenuView();
 		SettingsView settingsView = new SettingsView();
-		
+		PlayView playView = new PlayView();
+
 		// Set the references to this instance so that the views can call the change methods below
 		mainMenuView.setApp(this);
 		settingsView.setApp(this);
-		
+
 		cards.add(mainMenuView, "MainMenu");
 		cards.add(settingsView, "Settings");
-		
+		cards.add(playView, "Play");
+
 		setResizable(false);
 		setTitle(WINDOW_TITLE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
+
 		add(cards);
 		pack();
 	}
-	
+
 	public void swapView(String which) {
 		cardLayout.show(cards, which);
 	}
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Application a = new Application();
-                a.setVisible(true);
-            }
-        });
+			@Override
+			public void run() {
+				Application a = new Application();
+				a.setVisible(true);
+			}
+		});
 	}
 }
