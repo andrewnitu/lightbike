5b6a90363ae94f950504a0f01824b8ea9d0d62e6
Andrew Nitu <andrewnitu@gmail.com>
2017-03-13 04:59:01 -0400

    Add drawing method for SelectField

--- src/main/MenuDrawing.java
+++ src/main/MenuDrawing.java
@@ -63,4 +63,33 @@ public class MenuDrawing {
 			g2d.drawRect(xCoord1 - 10, yCoord1 - 10, xCoord2 - xCoord1 + 20, yCoord2 - yCoord1 + 20);
 		}
 	}
+	
+	public static void draw(SelectField e, Graphics2D g2d, boolean drawBorder) {
+		int xCoord1 = e.getxCoord1();
+		int xCoord2 = e.getxCoord2();
+		int yCoord1 = e.getyCoord1();
+		int yCoord2 = e.getyCoord2();
+
+		String text = e.getText();
+
+		Color buttonColor = e.getColor();
+
+		// drawing the button
+		g2d.setColor(buttonColor);
+		g2d.drawRect(xCoord1, yCoord1, xCoord2 - xCoord1, yCoord2 - yCoord1);
+
+		// drawing the button text
+		g2d.setFont(e.getFont());
+		FontMetrics metrics = g2d.getFontMetrics(e.getFont());
+		int stringWidth = metrics.stringWidth(text);
+		int stringHeight = metrics.getHeight();
+		int stringAscent = metrics.getAscent();
+		g2d.drawString(text, xCoord1 + (xCoord2 - xCoord1 - stringWidth) / 2,
+				yCoord1 + (yCoord2 - yCoord1 - stringHeight) / 2 + stringAscent);
+
+		// drawing (or erasing) the selection border
+		if (drawBorder) { // if not iterating on the current button, set the drawing colour to black
+			g2d.drawRect(xCoord1 - 10, yCoord1 - 10, xCoord2 - xCoord1 + 20, yCoord2 - yCoord1 + 20);
+		}
+	}
 }
