bfc57548cebef4c67d3742c8dd3f472182ecb500
davincornelius <davin@cleanhead.ca>
2017-03-19 21:28:02 -0400

    Change blue hue

--- src/main/Palette.java
+++ src/main/Palette.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 public class Palette {
 	public static final Color GREEN = new Color(0, 255, 0);
-	public static final Color BLUE = new Color(0, 0, 255);
+	public static final Color BLUE = new Color(50, 200, 255);
 	public static final Color RED = new Color(255, 0, 0);
 	public static final Color BLACK = new Color(0, 0, 0);
 	public static final Color WHITE = new Color(255, 255, 255);
