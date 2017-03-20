24f01b5247063be7d454c6a1c161b80a1b6a83e6
Andrew Nitu <andrewnitu@gmail.com>
2017-03-20 02:00:09 -0400

    Add score table on gameover screen

--- src/main/Application.java
+++ src/main/Application.java
@@ -100,7 +100,7 @@ public class Application extends JFrame {
 		
 		cards.add(gameOverView, "GameOver");
 		cardLayout.show(cards, "GameOver");
-		gameOverView.start();
+		gameOverView.start(results);
 	}
 
 	public static void main(String[] args) {
--- src/main/GameOverView.java
+++ src/main/GameOverView.java
@@ -22,15 +22,20 @@ public class GameOverView extends JPanel {
 	private final int BUTTON_HEIGHT = 50;
 
 	private final Font GAME_OVER_FONT = new Font("Helvetica", Font.BOLD, 128);
+	private final Font SUBHEADING_FONT = new Font("Helvetica", Font.BOLD, 60);
+	private final Font TABLE_ENTRY_FONT = new Font("Helvetica", Font.BOLD, 40);
 	private final Font BUTTON_FONT = new Font("Helvetica", Font.BOLD, 30);
 
 	private ArrayList<Button> buttonList = new ArrayList<Button>();
 	private int currentElement = 0;
 
 	private Application application;
+	
+	private ArrayList<Integer> results;
 
-	public void start() {
+	public void start(ArrayList<Integer> newResults) {
 		initializeGameOver();
+		results = newResults;
 		resume();
 	}
 
@@ -45,8 +50,8 @@ public class GameOverView extends JPanel {
 		setBackground(Palette.BLACK);
 		setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
 
-		Button back = new Button((WINDOW_WIDTH - BUTTON_WIDTH) / 2, 500,
-				(WINDOW_WIDTH - BUTTON_WIDTH) / 2 + BUTTON_WIDTH, 500 + BUTTON_HEIGHT, Palette.MAGENTA, "Main Menu",
+		Button back = new Button((WINDOW_WIDTH - BUTTON_WIDTH) / 2, 600,
+				(WINDOW_WIDTH - BUTTON_WIDTH) / 2 + BUTTON_WIDTH, 600 + BUTTON_HEIGHT, Palette.MAGENTA, "Main Menu",
 				BUTTON_FONT);
 
 		buttonList.add(back);
@@ -66,7 +71,7 @@ public class GameOverView extends JPanel {
 		if (Application.DEBUG)
 			System.out.println("Key pressed in GameOverView.java");
 
-		if (key == KeyEvent.VK_ENTER) {
+		if (key == KeyEvent.VK_ENTER || key == KeyEvent.VK_ESCAPE) {
 			if (currentElement == 0) {
 				backToMain();
 			}
@@ -99,9 +104,21 @@ public class GameOverView extends JPanel {
 
 		// draw in the game over text
 		FontMetrics titleMetrics = g.getFontMetrics(GAME_OVER_FONT);
+		FontMetrics subheadingMetrics = g.getFontMetrics(SUBHEADING_FONT);
 		g2d.setFont(GAME_OVER_FONT);
 		g2d.setColor(Palette.WHITE);
-		g2d.drawString("Game Over!", (WINDOW_WIDTH - titleMetrics.stringWidth("Game Over!")) / 2, WINDOW_HEIGHT / 3);
+		g2d.drawString("Game Over!", (WINDOW_WIDTH - titleMetrics.stringWidth("Game Over!")) / 2, WINDOW_HEIGHT / 5);
+		
+		
+		g2d.setFont(SUBHEADING_FONT);
+		g2d.drawLine((WINDOW_WIDTH - subheadingMetrics.stringWidth("Results")) / 2, WINDOW_HEIGHT / 3 - 60, (WINDOW_WIDTH + subheadingMetrics.stringWidth("Results")) / 2, WINDOW_HEIGHT / 3 - 60);
+		g2d.drawString("Results", (WINDOW_WIDTH - subheadingMetrics.stringWidth("Results")) / 2, WINDOW_HEIGHT / 3);
+		
+		g2d.setFont(TABLE_ENTRY_FONT);
+		for (int p = 0; p < results.size(); p++) {
+			g2d.drawString(p + 1 + "", WINDOW_WIDTH / 2 - 300, WINDOW_HEIGHT / 2 - 50 + 70 * p);
+			g2d.drawString("Player " + results.get(results.size() - 1 - p), WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 - 50 + 70 * p);
+		}
 
 		// iterate through each button and draw it
 		for (int i = 0; i < buttonList.size(); i++) {
