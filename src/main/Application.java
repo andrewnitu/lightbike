f8e599ae4592e2de3cc08001c568d1dd06b45da0
Andrew Nitu <andrewnitu@gmail.com>
2017-03-24 05:57:30 -0400

    Change the parameter pass on game over

--- src/main/Application.java
+++ src/main/Application.java
@@ -88,19 +88,19 @@ public class Application extends JFrame {
 		playView.start();
 	}
 	
-	public void swapGameOver(ArrayList<Integer> deads, ArrayList<Integer> ties) {
+	public void swapGameOver(DeathReport playerDeaths) {
 		gameOverView = new GameOverView();
 		gameOverView.setApp(this);
 		
 		if (DEBUG) {
 			for (int p = 0; p < players; p++) {
-				System.out.println("Player " + deads.get(p) + " took " + (players - p) + " place");
+				//System.out.println("Player " + deads.get(p) + " took " + (players - p) + " place");
 			}
 		}
 		
 		cards.add(gameOverView, "GameOver");
 		cardLayout.show(cards, "GameOver");
-		gameOverView.start(deads, ties);
+		//gameOverView.start(deads, ties);
 	}
 
 	public static void main(String[] args) {
